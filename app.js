9c548113984d14fabbb65e5f22001c0f8863f20f
discussion4all <discussion4all@yahoo.com>
2020-01-31 14:28:23 +0530

    added err

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get("/thumb", (req, res) => {
     (err, result) => {
       if (err) console.log("err", err);
       console.log("result", result);
-      res.send({ data: result });
+      res.send({ data: err });
     }
   );
 });
