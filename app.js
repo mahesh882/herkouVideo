332e8092d3f0ae55f207315be40ac3f6730f1154
discussion4all <discussion4all@yahoo.com>
2020-01-31 12:45:51 +0530

    added express to create server

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+const express = require("express");
 const ThumbnailGenerator = require("video-thumbnail-generator").default;
+const app = express();
 
-const tg = new ThumbnailGenerator({
-  sourcePath: "./SampleVideo_1280x720_1mb.mp4",
-  thumbnailPath: "./thumbnails",
-  tmpDir: "./" //only required if you can't write to /tmp/ and you need to generate gifs
+app.get("/", (req, res) => {
+  const tg = new ThumbnailGenerator({
+    sourcePath: "./SampleVideo_1280x720_1mb.mp4",
+    thumbnailPath: "./thumbnails",
+    tmpDir: "./" //only required if you can't write to /tmp/ and you need to generate gifs
+  });
+
+  tg.generateCb(
+    {
+      count: 1,
+      timemarks: ["3"]
+    },
+    (err, result) => {
+      if (err) console.log("err", err);
+      console.log(result);
+      res.send({ data: result });
+    }
+  );
 });
 
-tg.generateCb(
-  {
-    count: 1,
-    timemarks: ["3"]
-  },
-  (err, result) => {
-    if (err) console.log("err", err);
-    console.log(result);
-  }
-);
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
